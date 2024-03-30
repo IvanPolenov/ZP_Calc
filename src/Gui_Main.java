9560957592cc2ed8c523bedec2c251537d6005de
IvanPolenov <vpolenov9@gmail.com>
2024-03-30 11:18:49 +0500

    Pre-Alfa

--- src/Gui_Main.java
+++ src/Gui_Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Gui_Main {
+import javax.swing.*;
+
+class Gui_main extends JFrame {
+    private JTextField input0 = new JTextField(10);
+    private JTextField input1 = new JTextField(10);
+    private JTextField input2 = new JTextField(10);
+    private JRadioButton radioButton1 = new JRadioButton("Посуточная Оплата");
+    private JRadioButton radioButton2 = new JRadioButton("Почасовая оплата");
+    private JCheckBox checkBox = new JCheckBox("С учетом налогов?");
+    private JButton button = new JButton("Рассчитать");
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Gui_main app = new Gui_main();
+        app.setVisible(true);
     }
 }
