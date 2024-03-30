66d152dee35a245eff87900e340e5c4253755f62
IvanPolenov <vpolenov9@gmail.com>
2024-03-30 15:12:28 +0500

    pravka-1

--- Gui_main.java
+++ Gui_main.java
@@ -12,7 +12,27 @@ import javax.swing.JTextField;
         private JCheckBox checkBox = new JCheckBox("С учетом налогов?");
         private JButton button = new JButton("Рассчитать");
 
-        Gui_main() {
+        public Gui_main() {
+            System.out.println("Расчетный лист");
+            this.setBounds(100, 100, 280, 160);
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    }
-}
+            Container container = this.getContentPane();
+            container.setBackground(Color.PINK); // Изменение цвета на серый
+            container.setLayout(new GridLayout(5, 2, 2, 2));
+            container.add(new JLabel("Сотрудник:"));
+            container.add(input0);
+            container.add(new JLabel("Ставка:"));
+            container.add(input1);
+            container.add(new JLabel("Отработано часов:"));
+            container.add(input2);
+
+            ButtonGroup group = new ButtonGroup();
+            group.add(radioButton1);
+            group.add(radioButton2);
+            container.add(radioButton1);
+            container.add(radioButton2);
+            container.add(checkBox);
+            container.add(button);
+        }
+
